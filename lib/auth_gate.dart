ce37a3a2204f88d1a158e168678427bbc4f61f38
Charisse01 <chariscastillo21@gmail.com>
2024-11-12 22:44:47 -0400

    Verifying authentication status and section startup screen

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart'; // new
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -16,13 +17,40 @@ class AuthGate extends StatelessWidget {
          return SignInScreen(
            providers: [
              EmailAuthProvider(),
+             GoogleProvider(clientId: '130857452125-h08v16h040mdsms78str2ktqgprn57if.apps.googleusercontent.com'),  // new
            ],
            headerBuilder: (context, constraints, shrinkOffset) {
              return Padding(
                padding: const EdgeInsets.all(20),
                child: AspectRatio(
                  aspectRatio: 1,
-                 child: Image.asset('assets/flutterfire_300x.png'),
+                 child: Image.asset('flutterfire_300x.png'),
+               ),
+             );
+           },
+           subtitleBuilder: (context, action) {
+             return Padding(
+               padding: const EdgeInsets.symmetric(vertical: 8.0),
+               child: action == AuthAction.signIn
+                   ? const Text('Welcome to FlutterFire, please sign in!')
+                   : const Text('Welcome to Flutterfire, please sign up!'),
+             );
+           },
+           footerBuilder: (context, action) {
+             return const Padding(
+               padding: EdgeInsets.only(top: 16),
+               child: Text(
+                 'By signing in, you agree to our terms and conditions.',
+                 style: TextStyle(color: Colors.grey),
+               ),
+             );
+           },
+           sideBuilder: (context, shrinkOffset) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('flutterfire_300x.png'),
                ),
              );
            },
