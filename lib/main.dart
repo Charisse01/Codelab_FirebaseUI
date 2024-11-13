fbfd65e3d55ce09ccc873cee803da138ad75e403
Charisse01 <chariscastillo21@gmail.com>
2024-11-12 22:29:37 -0400

    Project created

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+ );
+
+
+ runApp(const MyApp());
+}
